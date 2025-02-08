061a2aa642df25620e2e539ec316a61eacfb2a05
Aarushi <aarushimandloi744@gmail.com>
2025-02-08 10:59:04 +0530

    3 Question

---
+++ swapTwoNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b); 
+    int c = a;
+    a = b;
+    b = c;
+    printf("a = %d\n",a);
+    printf("b = %d\n",b);
+    
+    return 0;
+} 
